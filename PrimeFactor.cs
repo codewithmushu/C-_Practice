55fca25a89f50821a255fb22c7200834d6563455
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-03-08 21:40:35 +0530

    [Muskan] Add : C sharp progeram to calculate prime factors for given input number.

---
+++ PrimeFactor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class PrimeFactor
+    {
+        public static void CalcPrimeFactor()
+        {
+            Console.WriteLine("Enter Number to calculate prime factors : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Prime factors of " + n + " are : ");
+            for (int i = 2; i <= n ; i++)
+            {
+                while ( n % i == 0 )
+                {
+                    Console.WriteLine( i + " ");
+                    n /= i;
+                }
+            }
+        }
+    }
+}
