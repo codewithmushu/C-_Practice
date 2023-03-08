1bc9501d2ff33677fa3c3ce9b933d95b3b7efe8c
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-03-08 16:47:21 +0530

    [Muskan] Add: C sharp program to calculate power of given input.

---
+++ CalcPowerOfTwo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class CalcPowerOfTwo
+    {
+        //This program takes a command-line argument N and prints a table of the
+        //powers of 2 that are less than or equal to 2^N.
+        public static void PowerOfTwo()
+        {
+            Console.WriteLine(" Please provide an integer value for Num : ");
+            int Num = Convert.ToInt32(Console.ReadLine());
+           // Console.WriteLine("Please provide power : ");
+           // int power = Convert.ToInt32(Console.ReadLine());
+            if (Num > 0)
+            {
+                Console.WriteLine("Powers of "+Num +" that are less than or equal to 2^N: ");
+                double result = 1;
+                for (int i = 0; i <= Num; i++) 
+                {
+                    result *= Num;
+                    Console.WriteLine($"{Num}^{i} = {result}");
+                }
+                
+            }
+        }
+    }
+}
