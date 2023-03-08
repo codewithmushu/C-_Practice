2a7c93f0af426852edb7479a1a6b94050dc6b64e
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-03-08 12:30:08 +0530

    [Muskan] Add : c sharp program to check given year is leap year or not.

---
+++ CheckLeapYear.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class CheckLeapYear
+    {
+        public static void Leapyearcalc()
+        {
+            Console.WriteLine("Enter Year : ");
+            int Year =Convert.ToInt32(Console.ReadLine());
+            if (Year % 4 == 0) 
+            {
+                Console.WriteLine("{0} is a Leap Year.", Year);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a Leap Year.", Year);
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
