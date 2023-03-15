588983a9e20dd6aa1c332359c3f6e9d37918ee5f
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-03-15 22:26:41 +0530

    [Muskan] Added : C# program to swap two numbers.

---
+++ SwapTwoNumbers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class SwapTwoNumbers
+    {
+        public static void DisplaySwappedNumbers()
+        {
+            int a = 87 , b = 45;
+
+            Console.WriteLine("Before Swap a = " +a+ " b = " + b);
+
+            a = a * b;
+            b = a / b;
+            a = a / b;
+
+            Console.WriteLine("After swap a = " + a + " b = " + b);
+            
+        }
+    }
+}
