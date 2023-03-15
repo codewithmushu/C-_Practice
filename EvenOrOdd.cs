5129147e4bb9a9d7cb4fcacb705a50dad668403c
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-03-15 22:52:15 +0530

    [Muskan] Added : C# program to check wheather a number is even or odd.

---
+++ EvenOrOdd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class EvenOrOdd
+    {
+        public static void CheckNumEvenOrOdd()
+        {
+            Console.WriteLine("Enter number to check wheather it is even or odd");
+            int num =Convert.ToInt32(Console.ReadLine());
+
+            if (num%2 == 0)
+            {
+                Console.WriteLine("{0} is even number.", num);
+            }
+            else
+            {
+                Console.WriteLine("{0} is odd number.", num);
+            }
+        }
+    }
+}
