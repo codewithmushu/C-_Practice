70b5d02a2a93d074b570e74573e80f8ed4a850f1
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-03-15 22:23:30 +0530

    [Muskan] Added : C# program to Compute Quotient and Remainder.

---
+++ QuotientAndRemainder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class QuotientAndRemainder
+    {
+        public static void FindQuotientAndRemainder()
+        {
+            int dividend = 67, divisor = 5;
+
+            int remainder = dividend % divisor;
+            int Quotient = dividend / divisor;
+
+            Console.WriteLine("The dividend {0} and advisor {1} ",dividend,divisor);
+            Console.WriteLine("The Quotient is " + Quotient + " The remainder is " + remainder);
+        }
+    }
+}
