a259f7bda016e4c819611b027c52901dee659337
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-03-15 22:50:29 +0530

    [Muskan] Added : some new programs.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Class1.username();
+            //Class2.Display();
+
+            //Flipcoin.Flipcoinpercentage();
+            //CheckLeapYear.Leapyearcalc();
+            //CalcPowerOfTwo.PowerOfTwo();
+            //NthHarmonicNumber.CalcNthHarmonicNumber();
+            // PrimeFactor.CalcPrimeFactor();
+            // VowelAndConsonant.CheckVowelAndConsonant();
+            //SwapTwoNumbers.DisplaySwappedNumbers();
+             //LargestNumber.FindLargestNum();
+            //QuotientAndRemainder.FindQuotientAndRemainder(); 
+            EvenOrOdd.CheckNumEvenOrOdd();
+        }
+    }
+}
