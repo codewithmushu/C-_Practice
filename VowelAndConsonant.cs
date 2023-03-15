2eccb1adea50ad81e2c9ea220c15c4eb31517565
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-03-15 22:31:34 +0530

    [Muskan] Added : C# program to check whether an alphabet is vowel or consonant.

---
+++ VowelAndConsonant.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class VowelAndConsonant
+    {
+        public static void CheckVowelAndConsonant()
+        {
+            char ch;
+
+            Console.WriteLine("Enter any character : ");
+            ch = Convert.ToChar(Console.ReadLine());
+
+            if ((ch =='a') || (ch == 'e') || (ch == 'i') || (ch == 'o') || (ch == 'u')
+            || (ch =='A') || (ch == 'E') || (ch == 'I') || (ch == 'O') || (ch == 'U')) 
+            {
+                Console.WriteLine(ch + " Is a vowel");
+
+            }
+            else if ((ch >=  'A' && ch <= 'Z')|| (ch >= 'a' && ch <= 'z'))
+            {
+                Console.WriteLine(ch + " Is a consonent ");
+            }
+        }
+       
+
+    }
+}
